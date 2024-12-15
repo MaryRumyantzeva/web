2c0d52ebee12cce9f8145655225bc229b55243f4
MaryRumyantzeva <rumyantzevamary@yandex.ru>
2024-12-15 21:33:26 +0300

    lab4

--- labs/lab 4/js/render.js
+++ labs/lab 4/js/render.js
@@ -7,19 +7,19 @@ const sortedDishes = dishes.sort((a, b) => a.name.localeCompare(b.name));
 
 // const fn = () => {}
 
-console.log(dishes)
-function renderDishes(element) {
-    const categories = {
-        soup: 'Суп',
-        mainDish: 'Главное блюдо',
-        drink: 'Напиток'
-    };
+const CATEGORIES = {
+    drink: 'Напиток',
+    mainDish: 'Главное блюдо',
+    soup: 'Суп',
+};
 
-    for (const category in categories) {
+function renderDishes(element) {
+
+    for (const category in CATEGORIES) {
         const dishSection = document.createElement('section');
         dishSection.classList.add('dishes');
         const header = document.createElement('h2');
-        header.textContent = categories[category];
+        header.textContent = CATEGORIES[category];
         dishSection.appendChild(header);
         const categoryDiv = document.createElement('div');
         categoryDiv.classList.add('category');
@@ -32,7 +32,7 @@ function renderDishes(element) {
                 <div data-dish="${dish.keyword}" class="dish">
                 <img src="${dish.image}" alt="${dish.name}" />
                 <div class="dish-content">
-                <p class="dish-price">${dish.price}</p>
+                <p class="dish-price">${dish.price}₽</p>
                 <p class="dish-title">${dish.name}</p>                    
                 <p class="dish-weight">${dish.count}</p>
                 <button class="add-button">Добавить</button>
@@ -47,13 +47,61 @@ function renderDishes(element) {
 }
 
 renderDishes(mainBlock)
-
-function setupAddButtons() {
+function totalSummary(order) {
+    let totalSum = 0;
+    const dishes = Object.values(order)
+    dishes.forEach(function (dish) {
+        if (dish !== null) {
+            totalSum += dish.price;
+        }
+    });
+    return totalSum
+}
+function setupForm() {
     const addButtons = document.querySelectorAll('.add-button');
-    addButtons.forEach(button => {
-        button.addEventListener('click', function () {
+    const order = {
+        soup: null,
+        mainDish: null,
+        drink: null,
+    }
+    const emptyOrderLabel = document.querySelector('.empty-order-label')
+    const labels = document.querySelectorAll('#dishes label[data-category]');
+    const totalSumElement = document.querySelector('#totalsum');
+    totalSumElement.parentElement.classList.add('hidden');
+    labels.forEach(label => label.classList.add('hidden'));
+    labels.forEach(label => {
+        const paragraph = document.createElement('p');
+        paragraph.innerHTML = 'Блюдо не выбрано';
+        paragraph.classList.add('empty-category')
+        paragraph.classList.add('hidden')
+        label.after(paragraph);
+    })
 
+    addButtons.forEach(button => {
+        button.addEventListener('click', (event) => {
+            emptyOrderLabel.classList.add('hidden')
+            totalSumElement.parentElement.classList.remove('hidden');
+            labels.forEach(label => label.classList.remove('hidden'));
+            const button = event.target;
+            const dishElement = button.parentElement.parentElement;
+            const dishKeyword = dishElement.dataset.dish
+            const dish = sortedDishes.find(value => value.keyword === dishKeyword);
+            order[dish.category] = dish;
+            labels.forEach(label => {
+                const labelCategory = label.dataset.category;
+                const paragraph = label.nextElementSibling;
+                paragraph.classList.remove('hidden')
+                if (labelCategory === dish.category) {
+                    const paragraphText = `${dish.name} ${dish.price}₽`
+                    paragraph.innerHTML = paragraphText;
+                }
+                paragraph.classList.add('empty-category')
+            })
+
+            totalSumElement.innerHTML = `${totalSummary(order)}₽`;
         });
     });
 }
 
+
+setupForm();
