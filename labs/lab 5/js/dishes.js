29447cbba26f7eb961657fe65fda049daaee33ee
MaryRumyantzeva <rumyantzevamary@yandex.ru>
2024-12-18 22:03:29 +0300

    1

---
+++ labs/lab 5/js/dishes.js
@@ -0,0 +1,302 @@
+export const dishes = [
+    {
+        keyword: 'gaspacho',
+        name: 'Гаспачо',
+        price: 195,
+        category: 'soup',
+        count: '350г',
+        image: 'images/gaspasho.jpg',
+        kind: 'veg'
+    },
+
+    {
+        keyword: 'ramen',
+        name: 'Рамен',
+        price: 375,
+        category: 'soup',
+        count: '425г',
+        image: 'images/ramen.jpg',
+        kind: 'fish'
+    },
+
+    {
+        keyword: 'tomyaam',
+        name: 'Том ям с креветками',
+        price: 650,
+        category: 'soup',
+        count: '500г',
+        image: 'images/tomyum.jpg',
+        kind: 'fish'
+    },
+
+    {
+        keyword: 'chicken',
+        name: 'Куриный суп',
+        price: 330,
+        category: 'soup',
+        count: '350г',
+        image: 'images/chicken.jpg',
+        kind: 'meat'
+    },
+
+    {
+        keyword: 'mushroom soup-pure',
+        name: 'Грибной суп-пюре',
+        price: 185,
+        category: 'soup',
+        count: '330г',
+        image: 'images/soup-pure.jpg',
+        kind: 'veg'
+    },
+
+    {
+        keyword: 'norwegian soup',
+        name: 'Норвежский суп',
+        price: 270,
+        category: 'soup',
+        count: '330г',
+        image: 'images/norvegski.jpg',
+        kind: 'meat'
+    },
+
+    {
+        keyword: 'fried potatoes with mushrooms',
+        name: 'Жареная картошка с грибами',
+        price: 150,
+        category: 'mainDish',
+        count: '250г',
+        image: 'images/freepotato.jpg',
+        kind: 'veg'
+    },
+
+    {
+        keyword: 'pizza margarita',
+        name: 'Пицца Маргарита',
+        price: 450,
+        category: 'mainDish',
+        count: '470г',
+        image: 'images/pizza.jpg',
+        kind: 'veg'
+    },
+
+    {
+        keyword: 'fish and rice',
+        name: 'Рыбная котлета с рисом и спаржей',
+        price: 320,
+        category: 'mainDish',
+        count: '270г',
+        image: 'images/fishrice.jpg',
+        kind: 'fish'
+    },
+
+    {
+        keyword: 'pasta with krevetki',
+        name: 'Паста с креветками',
+        price: 340,
+        category: 'mainDish',
+        count: '280г',
+        image: 'images/shrimppasta.jpg',
+        kind: 'fish'
+    },
+
+
+    {
+        keyword: 'lasagna',
+        name: 'Лазанья',
+        price: 385,
+        category: 'mainDish',
+        count: '310г',
+        image: 'images/lasania.jpg',
+        kind: 'meat'
+    },
+
+    {
+        keyword: 'chicken cutlets with potatoes',
+        name: 'Котлеты из курицы с картофельным пюре',
+        price: 225,
+        category: 'mainDish',
+        count: '280г',
+        image: 'images/pure.jpg',
+        kind: 'meat'
+    },
+
+    {
+        keyword: 'korean salat',
+        name: 'Корейский салат с овощами и яйцом',
+        price: 330,
+        category: 'salat',
+        count: '250г',
+        image: 'images/saladwithegg.jpg',
+        kind: 'veg'
+    },
+
+    {
+        keyword: 'cezar',
+        name: 'Цезарь с цыпленоком',
+        price: 370,
+        category: 'salat',
+        count: '220г',
+        image: 'images/caesar.jpg',
+        kind: 'meat'
+    },
+
+    {
+        keyword: 'kapreze with mozarella',
+        name: 'Капрезе с моцареллой',
+        price: 350,
+        category: 'salat',
+        count: '235г',
+        image: 'images/caprese.jpg',
+        kind: 'veg'
+    },
+
+    {
+        keyword: 'salat with tunez',
+        name: 'Салат с тунцом',
+        price: 480,
+        category: 'salat',
+        count: '250г',
+        image: 'images/tunasalad.jpg',
+        kind: 'fish'
+    },
+
+    {
+        keyword: 'freepotato with cesar',
+        name: 'Картофель фри с соусом Цезарь',
+        price: 350,
+        category: 'salat',
+        count: '235г',
+        image: 'images/frenchfries1.jpg',
+        kind: 'veg'
+    },
+
+    {
+        keyword: 'freepotato with ketchup',
+        name: 'Картофель фри с кетчупом',
+        price: 350,
+        category: 'salat',
+        count: '235г',
+        image: 'images/frenchfries2.jpg',
+        kind: 'veg'
+    },
+
+    {
+        keyword: 'orange juice',
+        name: 'Апельсиновый сок',
+        price: 120,
+        category: 'drink',
+        count: '300г',
+        image: 'images/sok-apel.jpg',
+        kind: 'cold'
+    },
+
+    {
+        keyword: 'apple juice',
+        name: 'Яблочный сок',
+        price: 90,
+        category: 'drink',
+        count: '300г',
+        image: 'images/sok-apple.jpg',
+        kind: 'cold'
+    },
+
+    {
+        keyword: 'carrot juice',
+        name: 'Морковный сок',
+        price: 110,
+        category: 'drink',
+        count: '300г',
+        image: 'images/sok-carrot.jpg',
+        kind: 'cold'        
+    },
+
+    {
+        keyword: 'black tea',
+        name: 'Черный чай',
+        price: 90,
+        category: 'drink',
+        count: '300г',
+        image: 'images/tea.jpg',
+        kind: 'hot'        
+    },
+
+    {
+        keyword: 'green tea',
+        name: 'Зеленый чай',
+        price: 100,
+        category: 'drink',
+        count: '300г',
+        image: 'images/greentea.jpg',
+        kind: 'hot'        
+    },
+
+    {
+        keyword: 'kapuchino',
+        name: 'Капучино',
+        price: 180,
+        category: 'drink',
+        count: '300г',
+        image: 'images/cappuccino.jpg',
+        kind: 'hot'        
+    },
+
+    {
+        keyword: 'pahlava',
+        name: 'Пахвала',
+        price: 220,
+        category: 'dessert',
+        count: '300г',
+        image: 'images/baklava.jpg',
+        kind: 'average-size'        
+    },
+
+    {
+        keyword: 'cheesecake',
+        name: 'Чизкейк',
+        price: 240,
+        category: 'dessert',
+        count: '125г',
+        image: 'images/checheesecake.jpg',
+        kind: 'small-size'        
+    },
+
+    {
+        keyword: 'choco cheesecake',
+        name: 'Шоколадный чизкейк',
+        price: 260,
+        category: 'dessert',
+        count: '125г',
+        image: 'images/chocolatecheesecake.jpg',
+        kind: 'small-size'        
+    },
+
+    {
+        keyword: 'chokocake',
+        name: 'Шоколадный торт',
+        price: 270,
+        category: 'dessert',
+        count: '140г',
+        image: 'images/chocolatecake.jpg',
+        kind: 'small-size'        
+    },
+
+    {
+        keyword: 'ponchiki3',
+        name: 'Пончики (3 штуки)',
+        price: 410,
+        category: 'dessert',
+        count: '350г',
+        image: 'images/donuts2.jpg',
+        kind: 'average-size'        
+    },
+
+    {
+        keyword: 'ponchiki6',
+        name: 'Пончики (6 штуки)',
+        price: 650,
+        category: 'dessert',
+        count: '700г',
+        image: 'images/donuts.jpg',
+        kind: 'big-size'        
+    },
+]
---
+++ labs/lab 5/js/render.js
@@ -0,0 +1,218 @@
+import { dishes } from "./dishes.js"
+
+const mainBlock = document.querySelector('main')
+const sortedDishes = dishes.sort((a, b) => a.name.localeCompare(b.name));
+
+// function fn() {}
+
+// const fn = () => {}
+
+const CATEGORIES = {
+    dessert: 'Десерт',
+    drink: 'Напиток',
+    salat: 'Салат',
+    mainDish: 'Главное блюдо',
+    soup: 'Суп',
+
+};
+
+const FILTERS = {
+    soup: [
+        { label: "рыбный", value: "fish" },
+        { label: "мясной", value: "meat" },
+        { label: "вегетарианский", value: "veg" }
+    ],
+    mainDish: [
+        { label: "рыбное", value: "fish" },
+        { label: "мясное", value: "meat" },
+        { label: "вегетарианское", value: "veg" }
+    ],
+    salat: [
+        { label: "рыбный", value: "fish" },
+        { label: "мясной", value: "meat" },
+        { label: "вегетарианский", value: "veg" }
+    ],
+    drink: [
+        { label: "холодный", value: "cold" },
+        { label: "горячий", value: "hot" },
+    ],
+    dessert: [
+        { label: "маленькая порция", value: "small-size" },
+        { label: "средняя порция", value: "average-size" },
+        { label: "большая порция", value: "big-size" }
+    ],
+};
+
+
+function renderDishes(element) {
+
+    for (const category in CATEGORIES) {
+        const dishSection = document.createElement('section');
+        dishSection.classList.add('dishes');
+
+        const header = document.createElement('h2');
+        header.textContent = CATEGORIES[category];
+        dishSection.appendChild(header);
+
+        const filterDiv = document.createElement("div");
+        filterDiv.classList.add("filters");
+
+        FILTERS[category].forEach((filter) => {
+            const filterButton = document.createElement("button");
+            filterButton.textContent = filter.label;
+            filterButton.dataset.kind = filter.value;
+            filterButton.classList.add("filter-button");
+            filterDiv.appendChild(filterButton);
+        });
+
+        dishSection.appendChild(filterDiv);
+
+        const categoryDiv = document.createElement('div');
+        categoryDiv.classList.add('category');
+        categoryDiv.setAttribute('data-category', category);
+
+        sortedDishes.forEach((dish) => {
+            if (dish.category === category) {
+                const dishDiv = document.createElement('div');
+                categoryDiv.appendChild(dishDiv);
+                dishDiv.outerHTML = `
+                <div data-dish="${dish.keyword}" data-kind=${dish.kind} class="dish">
+                <img src="${dish.image}" alt="${dish.name}" />
+                <div class="dish-content">
+                <p class="dish-price">${dish.price}₽</p>
+                <p class="dish-title">${dish.name}</p>                    
+                <p class="dish-weight">${dish.count}</p>
+                <button class="add-button">Добавить</button>
+                </div>
+                </div>`;
+            }
+        })
+        dishSection.appendChild(categoryDiv);
+        element.prepend(dishSection)
+    }
+
+}
+
+renderDishes(mainBlock)
+function totalSummary(order) {
+    let totalSum = 0;
+    const dishes = Object.values(order)
+    dishes.forEach(function (dish) {
+        if (dish !== null) {
+            totalSum += dish.price;
+        }
+    });
+    return totalSum
+}
+function setupForm() {
+    const addButtons = document.querySelectorAll('.add-button');
+
+    const order = {
+        soup: null,
+        mainDish: null,
+        drink: null,
+    }
+
+    const emptyOrderLabel = document.querySelector('.empty-order-label')
+    const labels = document.querySelectorAll('#dishes label[data-category]');
+    const totalSumElement = document.querySelector('#totalsum');
+
+    
+
+    totalSumElement.parentElement.classList.add('hidden');
+    labels.forEach(label => label.classList.add('hidden'));
+
+    labels.forEach(label => {
+        const paragraph = document.createElement('p');
+        const input = document.createElement('input');
+
+        input.name = label.dataset.category
+        input.classList.add('hidden');
+
+        paragraph.innerHTML = 'Блюдо не выбрано';
+        paragraph.classList.add('empty-category')
+        paragraph.classList.add('hidden')
+
+        label.after(paragraph);
+        paragraph.after(input);
+    })
+
+    addButtons.forEach(button => {
+        button.addEventListener('click', (event) => {
+            emptyOrderLabel.classList.add('hidden')
+            totalSumElement.parentElement.classList.remove('hidden');
+            labels.forEach(label => label.classList.remove('hidden'));
+
+            
+            
+            const button = event.target;
+            const dishElement = button.parentElement.parentElement;
+            const dishKeyword = dishElement.dataset.dish
+
+            const dish = sortedDishes.find(value => value.keyword === dishKeyword);
+            order[dish.category] = dish;
+
+            labels.forEach(label => {
+                const labelCategory = label.dataset.category;
+
+                const paragraph = label.nextElementSibling;
+                const input = paragraph.nextElementSibling;
+
+                paragraph.classList.remove('hidden')
+
+                if (labelCategory === dish.category) {
+                    const paragraphText = `${dish.name} ${dish.price}₽`
+                    paragraph.innerHTML = paragraphText;
+
+                    button.classList.add('button-active');   
+                    dishes.classList.add('dishes-active');
+                                   
+                    
+
+                    input.value = dish.keyword;
+                }
+                paragraph.classList.add('empty-category')
+            })
+
+            totalSumElement.innerHTML = `${totalSummary(order)}₽`;
+        });
+    });
+
+    const filterButtons = document.querySelectorAll(".filter-button");
+
+    filterButtons.forEach((button) => {
+        button.addEventListener("click", (event) => {
+            const clickedButton = event.target;
+            const kind = event.target.dataset.kind;
+            const categoryDiv = event.target.parentElement.parentElement;
+            const dishesFilter = categoryDiv.querySelectorAll(".dish");
+
+
+            if (clickedButton.classList.contains("active-btn")) {
+                clickedButton.classList.remove("active-btn");
+                dishesFilter.forEach((dish) => {
+                    dish.style.display = "block";
+                });
+            } else {
+                const allButtons = clickedButton.parentElement.querySelectorAll(".filter-button");
+                allButtons.forEach((btn) => btn.classList.remove("active-btn"));
+
+                clickedButton.classList.add("active-btn");
+
+
+                dishesFilter.forEach((dish) => {
+                    if (dish.dataset.kind === kind || !kind) {
+                        dish.style.display = "block";
+                    } else {
+                        dish.style.display = "none";
+                    }
+                });
+            }
+        });
+    });
+}
+
+
+setupForm();
+
+
